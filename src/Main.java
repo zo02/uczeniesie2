9af6f393e33fef3f72ed08f1f45d470b767d645b
Zofia Borys <zosiaborys.zb@gmail.com>
2024-10-10 22:52:35 +0200

    fix/fixed tescik Fixed tescik.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Register!");
         System.out.println("Register!");
         System.out.println("Register");
+        System.out.println("Teścik");
     }
 
 }
